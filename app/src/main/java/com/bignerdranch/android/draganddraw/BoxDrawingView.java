4bf0d5fd43ba1ba2f9ad51e129d7b30bb17f72f0
unknown <caldwellc1@CS-317-9KHKFQ1.appd.appstate.edu>
2016-11-10 11:37:14 -0500

    Color&Clear Update

--- app/src/main/java/com/bignerdranch/android/draganddraw/BoxDrawingView.java
+++ app/src/main/java/com/bignerdranch/android/draganddraw/BoxDrawingView.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.PointF;
+import android.os.Bundle;
+import android.os.Parcelable;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
@@ -17,10 +19,11 @@ import java.util.List;
  */
 public class BoxDrawingView extends View {
     private static final String TAG = "BoxDrawingView";
+    private static final String PARENT_STATE_KEY = "ParentKey?";
     private Box mCurrentBox;
-    private List<Box> mBoxen = new ArrayList<>();
-    private Paint mBoxPaint;
-    private Paint mBackgroundPaint;
+    public static List<Box> mBoxen = new ArrayList<>();
+    public static Paint mBoxPaint;
+    public static Paint mBackgroundPaint;
 
     public BoxDrawingView(Context context){
         this(context, null);
@@ -77,4 +80,44 @@ public class BoxDrawingView extends View {
         Log.i(TAG, action + " at x=" + current.x + ", y=" + current.y);
         return true;
     }
+
+    @Override
+    protected Parcelable onSaveInstanceState(){
+        Parcelable parentParcelable = super.onSaveInstanceState();
+        Bundle bundle = new Bundle();
+        bundle.putParcelable(PARENT_STATE_KEY, parentParcelable);
+
+        int boxNumber =1;
+        for(Box box : mBoxen){
+            float[] pointArray= {box.getOrigin().x, box.getOrigin().y, box.getCurrent().x, box.getCurrent().y};
+            bundle.putFloatArray("box" + boxNumber, pointArray);
+            boxNumber++;
+        }
+
+        return bundle;
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Parcelable state){
+        if(state instanceof Bundle) {
+            Bundle bundle = (Bundle) state;
+            super.onRestoreInstanceState(bundle.getParcelable(PARENT_STATE_KEY));
+            String prefixName = "box";
+            int boxCount = 1;
+            while (bundle.containsKey(prefixName + boxCount)) {
+                float[] pointArray = bundle.getFloatArray(prefixName + boxCount);
+                PointF origin = new PointF(pointArray[0], pointArray[1]);
+                PointF current = new PointF(pointArray[2], pointArray[3]);
+                Box box = new Box(origin);
+                box.setCurrent(current);
+
+                mBoxen.add(box);
+                boxCount++;
+            }
+        }
+        else{
+            super.onRestoreInstanceState(state);
+        }
+    }
+
 }
--- app/src/main/java/com/bignerdranch/android/draganddraw/DragAndDrawFragment.java
+++ app/src/main/java/com/bignerdranch/android/draganddraw/DragAndDrawFragment.java
@@ -3,6 +3,9 @@ package com.bignerdranch.android.draganddraw;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -10,6 +13,8 @@ import android.view.ViewGroup;
  * Created by caldwellc1 on 11/8/2016.
  */
 public class DragAndDrawFragment extends Fragment {
+    private BoxDrawingView mBoxDrawingView;
+
     public static DragAndDrawFragment newInstance(){
         return new DragAndDrawFragment();
     }
@@ -17,6 +22,42 @@ public class DragAndDrawFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         View v = inflater.inflate(R.layout.fragment_drag_and_draw, container, false);
+        mBoxDrawingView = (BoxDrawingView) v.findViewById(R.id.View);
         return v;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater){
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.fragment_options, menu);
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch(item.getItemId()){
+            case R.id.menu_item_color:
+                //BoxDrawingView boxDrawingView = BoxDrawingView.get(getActivity());
+                if(BoxDrawingView.mBoxPaint.getColor()==0x22ff0000){
+                    //BoxDrawingView.mBoxPaint.setColor(getResources().getColor(R.color.otherbox));
+                    BoxDrawingView.mBoxPaint.setColor(0x2200ffff);
+                    BoxDrawingView.mBackgroundPaint.setColor(0xff000000);
+                }
+                else {
+                    BoxDrawingView.mBoxPaint.setColor(0x22ff0000);
+                    BoxDrawingView.mBackgroundPaint.setColor(0xfff8efe0);
+                }
+                return true;
+            case R.id.menu_item_clear:
+                BoxDrawingView.mBoxen.clear();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
